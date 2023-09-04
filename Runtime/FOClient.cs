66a1ca9b800e1045f84e1e04b964c44cd355794e
hudmarc <44267994+hudmarc@users.noreply.github.com>
2023-09-04 18:41:18 +0200

    Update FOClient.cs

--- Runtime/FOClient.cs
+++ Runtime/FOClient.cs
@@ -6,24 +6,12 @@ namespace FishNet.FloatingOrigin
     [RequireComponent(typeof(NetworkObject))]
     public class FOClient : FOObject
     {
-        [SerializeField] private bool complainIfMoved = false;
         Vector3 init;
         internal override void Initialize() => FOManager.instance.RegisterClient(this);
         internal override void Deinitialize() => FOManager.instance.UnregisterClient(this);
         void Start()
         {
             init = transform.position;
-        }
-        void Update()
-        {
-            if (complainIfMoved)
-            {
-                if (FOManager.instance.UnityToReal(transform.position, gameObject.scene) != FOManager.instance.UnityToReal(init, gameObject.scene))
-                {
-                    Debug.LogError("I was moved!");
-                    Debug.Break();
-                }
-            }
         }
     }
 }
